c0a78eb92267468bc11c3d8ad9963ee3881008a4
Kidlike <steve.tcmg@gmail.com>
2022-07-29 17:42:15 +0200

    mn create-app --build=maven mn-form-urlencoded-jackson

---
+++ src/main/java/mn/form/urlencoded/jackson/Application.java
@@ -0,0 +1,9 @@
+package mn.form.urlencoded.jackson;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
